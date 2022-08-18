c282e71a2e247f66a2d5751d7079550b0d37ab2f
Coulsen Anderson <coulsen.anderson@gmail.com>
2022-08-17 20:28:17 -0600

    lab-wednesday-week1-v1

--- lab.js
+++ lab.js
@@ -116,6 +116,11 @@ let passingScore = 7;
 */
 
 //CODE HERE
+// while (score < 7){
+//   console.log('Your score is not high enough')
+//   score++
+// }
+
 
 ////////// INTERMEDIATE PROBLEMS //////////
 
@@ -135,12 +140,15 @@ if(changeMyMind){
   changeMyMind = true
 }
 
-console.log(changeMyMind)
+// console.log(changeMyMind)
 
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
 //CODE HERE
+changeMyMind = !changeMyMind
+
+console.log(changeMyMind)
 
 ////////// ADVANCED PROBLEMS //////////
 
@@ -150,9 +158,9 @@ let z = 5;
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
-while(z > 0){
-  for(let i = z; i > 0; i--){
-    console.log(i)
-  }
-  z--
-}
+// while(z > 0){
+//   for(let i = z; i > 0; i--){
+//     console.log(i)
+//   }
+//   z--
+// }
